973d514821d6d67714a58b687f3a9cacabcb253b
zeroiszero <20971727+zeroiszero@users.noreply.github.com>
2021-08-15 23:19:54 +0800

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = (phase, { defaultConfig }) => {
+    /**
+     * @type {import('next').NextConfig}
+     */
+    const nextConfig = {
+        /* https://nextjs.org/docs/api-reference/next.config.js/introduction */
+        /* config options here */
+    }
+    return nextConfig
+}
